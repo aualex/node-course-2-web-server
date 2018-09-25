377798348f19dcaf4ef57405f9fbb310eda22191
IBM Cloud Garage <ibmcloudgarage@Lavanyas-MacBook-Pro.local>
2018-09-25 16:54:18 -0700

    header.hds changes

--- server.js
+++ server.js
@@ -56,7 +56,14 @@ app.get('/about', (req, res) => {
         authorName: 'Alex'
     });
 });
-
+app.get('/project', (req, res) => {
+    // res.send('about page here...');
+    res.render('project.hbs', {
+        pageTitle: 'Project page',
+        // currentYear: new Date().getFullYear(),
+        authorName: 'Alex'
+    });
+});
 app.get('/bad', (req, res) => {
     res.send({
         errorCode: 404,
